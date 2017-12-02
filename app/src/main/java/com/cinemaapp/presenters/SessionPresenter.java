4930dc0f00aa716415d21ef16f5d9a8a15e78b41
jasmany <zeroblack2050@hotmail.com>
2017-12-01 23:11:46 -0500

    cinema

---
+++ app/src/main/java/com/cinemaapp/presenters/SessionPresenter.java
@@ -0,0 +1,8 @@
+package com.cinemaapp.presenters;
+
+/**
+ * Created by jasmany on 28/11/2017.
+ */
+
+public class SessionPresenter {
+}
---
+++ app/src/main/java/com/cinemaapp/views/billboard/ISessionWithTwitter.java
@@ -0,0 +1,8 @@
+package com.cinemaapp.views.billboard;
+
+/**
+ * Created by jasmany on 28/11/2017.
+ */
+
+public interface ISessionWithTwitter {
+}
